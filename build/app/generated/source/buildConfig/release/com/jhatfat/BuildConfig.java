4a351c34fa2190d5a5fdc2ab7f4c41d49657bc19
meghagunjanmg <meghagunjanmg@gmail.com>
2023-01-15 20:05:16 +0530

    14-01

--- build/app/generated/source/buildConfig/release/com/jhatfat/BuildConfig.java
+++ build/app/generated/source/buildConfig/release/com/jhatfat/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "com.jhatfat";
   public static final String BUILD_TYPE = "release";
-  public static final int VERSION_CODE = 35;
-  public static final String VERSION_NAME = "35.0";
+  public static final int VERSION_CODE = 36;
+  public static final String VERSION_NAME = "36.0";
 }
